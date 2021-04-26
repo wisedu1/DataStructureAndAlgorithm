de0b9f2fb30e9351c3f528ca7909f5adb184aa83
wisedu1 <wisedu1@gmail.com>
2021-04-26 16:01:24 +0800

    update offer solution 58_2

--- src/offer/easy/no58_2/Solution.java
+++ src/offer/easy/no58_2/Solution.java
@@ -5,14 +5,15 @@ package offer.easy.no58_2;
  */
 class Solution {
     public String reverseLeftWords(String s, int n) {
-        char[] chars = s.toCharArray();
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < chars.length - 1; j++) {
-                char temp = chars[j];
-                chars[j] = chars[j + 1];
-                chars[j + 1] = temp;
-            }
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = n; i < s.length(); i++) {
+            stringBuilder.append(s.charAt(i));
         }
-        return String.valueOf(chars);
+        for (int i = 0; i < n; i++) {
+            stringBuilder.append(s.charAt(i));
+        }
+
+        return stringBuilder.toString();
     }
 }
+
